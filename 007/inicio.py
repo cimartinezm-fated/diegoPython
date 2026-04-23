5623f596f6746514a847407b70b3e9016d327f78
Diegorr <diegoroblesrivera@gmail.com>
2026-04-23 15:33:20 -0400

    ejercicios con strings

--- 007/inicio.py
+++ 007/inicio.py
@@ -41,17 +41,72 @@
 
 #Uso de f string
 
-titulo="Clima de hoy"  # String
-diaDelMes=13  # int
-mes=4       #int
+# titulo="Clima de hoy"  # String
+# diaDelMes=13  # int
+# mes=4       #int
 
-temperatura=22.3 # float
+# temperatura=22.3 # float
 
-llueve=False # boolean
+# llueve=False # boolean
 
-print(titulo,"temperatura actual:", temperatura, "al dia",diaDelMes, "/", mes )
-print(f"{titulo} temperatura actual: {temperatura} al dia {diaDelMes}/{mes}")
+# print(titulo,"temperatura actual:", temperatura, "al dia",diaDelMes, "/", mes )
+# print(f"{titulo} temperatura actual: {temperatura} al dia {diaDelMes}/{mes}")
+# print(titulo +"temperatura actual:" )
+
+
+# print("Hola Daniel "*3)
+# n="daniel"
+# print(n[-12])
+
+
+name1="Catalina Pinochet"
+print(len(name1))
+# name2=" Vicente "
+
+# print(name2.strip())
+# print(name1.lower())
+# print(name1.upper())
+# print(name1.replace("Pinochet", "Frei"))
+# print(name1.find("Pinochet"))
 
 
 
+'''Pedir a la clave al usuario y verificar 
+que sea SHAZAM independiente de su case
+(sin inporta si son mayusculas o minusculas)'''
+
+# clave="SHAZAM"
+
+# nom=input("Ingresá la clave: ")
+
+# if nom.upper() == clave:
+#     print("Clave correcta")
+# else:
+#     print("Clave invalida")
+
+'''Crear un nombre de usuario y verificar su que 
+su largo esté entre 4 y 10 caracteres
+'''
+
+nombre=input("Ingrese su nombre de usuario: ")
+
+if 4<=len(nombre)<=10 :
+    print("Usuario correcto")
+else:
+    print("Usuario fuera de rango")
+
+'''Cear un pin y que este tenga exactamente
+4 digitos'''
+
+pin=int(input("Ingrese un pin de 4 digitos"))
+542452345234532532
+if len(str(pin))==4:
+    print("Pin creado")
+else:
+    print("Pin NO creado")
+
+if 1000<=pin<=9999:
+    print("Pin creado")
+else:
+    print("Pin NO creado")
 
